1b2c040b6a5ae60c0b1760de61c57663f42578eb
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2025-04-30 16:39:55 +0200

    Added styling

--- src/App.tsx
+++ src/App.tsx
@@ -25,23 +25,23 @@ export default function App() {
   return (
     <section className="flex flex-col gap-2.5">
 
-      <div className="flex flex-col gap-5 border border-black w-[400px] p-5">
-        <img src="Images/sushi_mascot-removebg-preview.png" className="border border-red-600"/>
+      <div className="flex flex-col gap-5 bg-amber-50 border-4 border-black w-[400px] px-5 py-7 rounded-2xl">
+        <img src="Images/sushi_mascot-removebg-preview.png"/>
         <div className="text-5xl flex justify-evenly">
-          <button className="flex justify-center items-center border border-black rounded-full h-[60px] w-[60px]"
+          <button className="flex justify-center items-center text-white bg-red-300 border-4 border-black rounded-full h-[60px] w-[60px] hover:bg-red-200"
             onClick={()=> {decreaseCount()}}
           >-</button>
 
-          <div className="flex justify-center items-center border border-black rounded-2xl w-[100px] h-[60px]"
+          <div className="flex justify-center items-center bg-white border-4 border-black rounded-2xl w-[100px] h-[60px]"
           >{count}</div>
 
-          <button className="flex justify-center items-center border border-black rounded-full h-[60px] w-[60px]"
+          <button className="flex justify-center items-center text-white bg-red-300 border-4 border-black rounded-full h-[60px] w-[60px] hover:bg-red-200"
             onClick={()=> {increaseCount()}}
           >+</button>
         </div>
       </div>
 
-      <div className="high-score border border-black">{`HIGH SCORE : ${highScore}`}</div>
+      <div className="high-score text-4xl text-white flex justify-center items-center rounded-2xl h-[80px] border-4 bg-red-300 border-black">{`HIGH SCORE : ${highScore}`}</div>
 
     </section>
   )
